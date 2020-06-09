3755531c575a3944fcdcc565d1bd22de82132111
Lucas Nogueira <lucas_snogueira@hotmail.com>
2020-06-09 00:22:03 -0300

    feat(*): upgrade @types/react-native version and create default button component

--- src/pages/Signin/styles.ts
+++ src/pages/Signin/styles.ts
@@ -1,6 +1,12 @@
 import styled from 'styled-components/native';
 import { TextInput as MaterialTextInput } from 'react-native-paper';
 
+import { TextInputProps } from 'react-native-paper/lib/typescript/src/components/TextInput/TextInput';
+
+interface InputProps extends TextInputProps {
+  name: string;
+}
+
 export const Container = styled.View`
   flex: 1;
   align-items: center;
@@ -13,6 +19,8 @@ export const LogoWrapper = styled.View`
   flex-direction: row;
   align-items: center;
   justify-content: center;
+
+  margin-bottom: 40px;
 `;
 
 export const LogoText = styled.Text`
@@ -24,7 +32,33 @@ export const LogoText = styled.Text`
   margin: 8px 0 0 15px;
 `;
 
-export const TextInput = styled(MaterialTextInput)`
+export const TextInput = styled(MaterialTextInput)<InputProps>`
   width: 100%;
   background-color: #fff;
+`;
+
+export const WelcomeText = styled.Text`
+  font-family: 'Ubuntu-Regular';
+  font-size: 36px;
+  margin-bottom: 7px;
+  align-self: flex-start;
+`;
+
+export const SubText = styled.Text`
+  font-family: 'Ubuntu-Regular';
+  font-size: 16px;
+  color: #747474;
+  margin-bottom: 62px;
+  align-self: flex-start;
+`;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin: 24px 0 37px;
+  align-self: flex-end;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: #626262;
+  font-size: 16px;
+  font-family: 'Roboto-Regular';
 `;
